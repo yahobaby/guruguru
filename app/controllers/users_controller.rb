54c30c4f468c5e1eb333a2f3054168c753f39ab2
yahobaby <110825175+yahobaby@users.noreply.github.com>
2022-11-24 16:11:42 +0900

    投稿者のマイページにアクセス

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,7 @@ class UsersController < ApplicationController
     user = User.find(params[:id])
     # infoの右下に表示されるユーザー名をクリックすることで、送られたidをparamsで取得、そのユーザーのレコードを変数userに代入。
     # この変数を利用して、各インスタンス変数ではcurrent_userを変数userに変える。
-    @nickname = current_user.nickname #「ログイン中のユーザーのnickname」 
-    @infos = current_user.infos #「ログイン中のユーザーのinfo投稿」、アソシエーションを利用しuser.infosとすることで、そのユーザーが投稿したinfoを取得して、@infosに代入
+    @nickname = user.nickname #「投稿したユーザーのnickname」 
+    @infos = user.infos #「投稿したユーザーのinfo投稿」、アソシエーションを利用しuser.infosとすることで、そのユーザーが投稿したinfoを取得して、@infosに代入
   end
 end
